4a8e899984654558d0b323b0980fa173f57517d1
Tung Dong Viet <fizzy_martin@protonmail.com>
2025-10-11 12:12:20 +0700

    Add communication through callbacks

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+
+  history.listen(onNavigate);
+
   ReactDOM.render(
-    <App/>,
+    <App history={history} />,
     el
   );
 };
