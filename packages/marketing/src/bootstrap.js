67ba48ead28ac7641c2776b4fe37c08b5d99da06
VietTungFizzy <fizzy_martin@protonmail.com>
2025-10-18 15:21:46 +0700

    Add Memory History and Browser History

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
 
-  history.listen(onNavigate);
+  if(onNavigate) {
+    history.listen(onNavigate);
+  }
 
   ReactDOM.render(
     <App history={history} />,
     el
   );
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if(pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 
@@ -22,7 +34,7 @@ if(process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if(devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
